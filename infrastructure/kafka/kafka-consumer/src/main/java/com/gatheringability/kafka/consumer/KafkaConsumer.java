b610a3ed99b5f1cac81cf772f518c2b6d63ce1c6
Roger Turnau <rturnau@gmail.com>
2024-04-28 14:58:54 -0400

    committing all files prior to creating data module for order system

--- infrastructure/kafka/kafka-consumer/src/main/java/com/gatheringability/kafka/consumer/KafkaConsumer.java
+++ infrastructure/kafka/kafka-consumer/src/main/java/com/gatheringability/kafka/consumer/KafkaConsumer.java
@@ -2,6 +2,8 @@ package com.gatheringability.kafka.consumer;
 
 import org.apache.avro.specific.SpecificRecordBase;
 
+import java.util.List;
+
 public interface KafkaConsumer<T extends SpecificRecordBase> {
-   // void receive(List<T> messages, List<Long> keys, List<>)
+    void receive(List<T> messages, List<Long> keys, List<Integer> partitions, List<Long> offsets);
 }
