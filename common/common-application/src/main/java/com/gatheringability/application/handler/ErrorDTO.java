a507e2f3d3e88f0790b65c6913562bcf22639fe1
Roger Turnau <rturnau@gmail.com>
2024-04-28 15:01:38 -0400

    adding files missed from previous commit

---
+++ common/common-application/src/main/java/com/gatheringability/application/handler/ErrorDTO.java
@@ -0,0 +1,13 @@
+package com.gatheringability.application.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ErrorDTO {
+    private final String code;
+    private final String message;
+}
---
+++ common/common-application/src/main/java/com/gatheringability/application/handler/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.gatheringability.application.handler;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value={Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDTO handleException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ErrorDTO.builder().code(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .message("Unexpected error!").build();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value={ValidationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleException(ValidationException validationException) {
+        ErrorDTO errorDTO;
+        if (validationException instanceof ConstraintViolationException) {
+            String violations = extractVioloationsFromException((ConstraintViolationException) validationException);
+            log.error(violations, validationException);
+            errorDTO = ErrorDTO.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(violations)
+                    .build();
+        } else {
+            String exceptionMessage = validationException.getMessage();
+            log.error(exceptionMessage, validationException);
+            errorDTO = ErrorDTO.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(exceptionMessage)
+                    .build();
+        }
+        return errorDTO;
+    }
+
+    private String extractVioloationsFromException(ConstraintViolationException validationException) {
+        return validationException.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("--"));
+    }
+}
